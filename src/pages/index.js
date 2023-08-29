702db0afc53da373d8a2785707ad1777ac5651c1
Ajay Krishna R <balaajay19@gmail.com>
2023-08-29 16:36:27 +0530

    feat: Added carousel component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       {/* Header */}
-      <Header/>
+      <Header />
 
-      <h1>Hello World!</h1>
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        Hello
+        {/* ProductFeed */}
+      </main>
     </div>
   );
 }
