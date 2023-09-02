8b108d9a8a388ee8a785c5da7381457a5cb1780b
Ajay Krishna R <balaajay19@gmail.com>
2023-09-02 20:25:55 +0530

    feat: Added product component & few fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,13 +22,89 @@ function Header() {
         </div>
 
         {/* Top nav search */}
-        <div className="hidden sm:flex  w-40 m-auto focus-within:flex-grow items-center h-10 rounded-md cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+        <div className="hidden sm:flex  w-40 m-auto items-center h-10 rounded-md cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <select
+            aria-describedby="searchDropdownDescription"
+            className="h-full bg-gray-200 rounded-sm w-11 text-sm text-gray-600 rounded-l-sm"
+            data-nav-selected="0"
+            id="searchDropdownBox"
+            name="url"
+            tabindex="0"
+            title="Search in"
+          >
+            <option selected="selected" value="search-alias=aps">
+              All
+            </option>
+            <option value="search-alias=alexa-skills">Alexa Skills</option>
+            <option value="search-alias=amazon-devices">Amazon Devices</option>
+            <option value="search-alias=fashion">Amazon Fashion</option>
+            <option value="search-alias=nowstore">Amazon Fresh</option>
+            <option value="search-alias=amazon-pharmacy">
+              Amazon Pharmacy
+            </option>
+            <option value="search-alias=appliances">Appliances</option>
+            <option value="search-alias=mobile-apps">Apps &amp; Games</option>
+            <option value="search-alias=audible">Audible Audiobooks</option>
+            <option value="search-alias=baby">Baby</option>
+            <option value="search-alias=beauty">Beauty</option>
+            <option value="search-alias=stripbooks">Books</option>
+            <option value="search-alias=automotive">Car &amp; Motorbike</option>
+            <option value="search-alias=apparel">
+              Clothing &amp; Accessories
+            </option>
+            <option value="search-alias=collectibles">Collectibles</option>
+            <option value="search-alias=computers">
+              Computers &amp; Accessories
+            </option>
+            <option value="search-alias=electronics">Electronics</option>
+            <option value="search-alias=furniture">Furniture</option>
+            <option value="search-alias=lawngarden">
+              Garden &amp; Outdoors
+            </option>
+            <option value="search-alias=gift-cards">Gift Cards</option>
+            <option value="search-alias=grocery">
+              Grocery &amp; Gourmet Foods
+            </option>
+            <option value="search-alias=hpc">Health &amp; Personal Care</option>
+            <option value="search-alias=kitchen">Home &amp; Kitchen</option>
+            <option value="search-alias=industrial">
+              Industrial &amp; Scientific
+            </option>
+            <option value="search-alias=jewelry">Jewellery</option>
+            <option value="search-alias=digital-text">Kindle Store</option>
+            <option value="search-alias=luggage">Luggage &amp; Bags</option>
+            <option value="search-alias=luxury-beauty">Luxury Beauty</option>
+            <option value="search-alias=dvd">Movies &amp; TV Shows</option>
+            <option value="search-alias=popular">Music</option>
+            <option value="search-alias=mi">Musical Instruments</option>
+            <option value="search-alias=office-products">
+              Office Products
+            </option>
+            <option value="search-alias=pets">Pet Supplies</option>
+            <option value="search-alias=instant-video">Prime Video</option>
+            <option value="search-alias=shoes">Shoes &amp; Handbags</option>
+            <option value="search-alias=software">Software</option>
+            <option value="search-alias=sporting">
+              Sports, Fitness &amp; Outdoors
+            </option>
+            <option value="search-alias=specialty-aps-sns">
+              Subscribe &amp; Save
+            </option>
+            <option value="search-alias=home-improvement">
+              Tools &amp; Home Improvement
+            </option>
+            <option value="search-alias=toys">Toys &amp; Games</option>
+            <option value="search-alias=under-ten-dollars">Under ₹500</option>
+            <option value="search-alias=videogames">Video Games</option>
+            <option value="search-alias=watches">Watches</option>
+          </select>
           <input
             type="text"
-            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            className="p-2 h-full w-6 flex-grow flex-shrink focus:outline-none px-4"
             autoComplete="true"
             name="prodsearch"
             id="prodsearch"
+            placeholder="Search Amazon Clone !!!"
           />
           <SearchIcon className="h-12 p-4" />
         </div>
@@ -47,7 +123,7 @@ function Header() {
             <p>Sign In -&gt;</p>
             <UserCircleIcon className="h-7 pl-2" />
           </div>
-          <div className="relative cursor-pointer flex items-end pr-10 sm:pr-0">
+          <div className="relative cursor-pointer flex items-end pr-10 sm:pr-0 hover:outline rounded-sm outline-1 outline-white">
             <span className="absolute bottom-5 flex justify-center place-items-center right-6 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
               0
             </span>
---
+++ src/components/Product.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute  top-3 right-3 text-xs italic font-semibold p-1 rounded-sm bg-green-400">
+        {category}
+      </p>
+
+      <Image
+        src={image}
+        height={200}
+        width={200}
+        style={{ objectFit: "contain", alignSelf: "center", marginTop: "1rem" }}
+      />
+
+      <h4 className="my-3">{title}</h4>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-4 text-yellow-500" />
+          ))}
+      </div>
+
+      <div className="text-xs my-2 line-clamp-2">{description}</div>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="GBP" />
+      </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img
+            className="w-12"
+            src="https://img.icons8.com/color/48/amazon-prime.png"
+            alt="Prime Logo"
+          />
+          <p className="text-xs text-gray-500">FREE Next-Day Delivery</p>
+        </div>
+      )}
+
+      <button class="mt-auto button">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
